beb950d107bf3aede41199912e3355f37fc8743c
Noah Sun <934937054@qq.com>
2017-07-05 01:43:53 +0800

    添加tsconfig include选项
    添加include以后ts文件里面可以直接使用已经安装过@types的声明文件，不需要再引用

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,9 @@
-// / <reference path="../../node_modules/@types/jquery/index.d.ts" />
+// 在tsconfig.json中include了@types的声明文件后可不需要下面两种方式的引用
+// /// <reference path="../../node_modules/@types/jquery/index.d.ts" />
+// import * as $ from "jquery";
+
 import "font-awesome/css/font-awesome.min.css";
 import "../public/css/index.scss";
-import * as $ from "jquery";
 
 class Person {
     constructor(public name: string) {
