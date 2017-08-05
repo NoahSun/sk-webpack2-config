ca8133a8c36f474d99a843f9ecd2ba8dab2d02df
Noah Sun <934937054@qq.com>
2017-08-05 22:09:19 +0800

    libraryTarget默认为umd，可直接在浏览器中运行

--- webpack.base.js
+++ webpack.base.js
@@ -20,7 +20,7 @@ module.exports = {
         filename: '[name].bundle.js',
         path: path.join(__dirname, 'dist'),
         library: '[name]',
-        libraryTarget: 'commonjs' // 'amd' | 'commonjs' | 'umd'
+        libraryTarget: 'umd' // 'amd' | 'commonjs' | 'umd'
     },
     resolve: {
         extensions: ['.json', '.js', '.jsx', '.ts', '.tsx', '.css', '.scss']
